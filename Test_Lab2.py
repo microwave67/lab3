69c69fe9a7e71a893fc67be3e0c14219d761fe5f
Jovian Heng <jovianheng.24@ichat.sp.edu.sg>
2025-11-11 16:34:05 +0800

    Added lab2 test file

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -1,8 +1,30 @@
+from lab2.lab2b import find_min_max
+from lab2.lab2b import calc_average
+from lab2.lab2b import calc_median_temperature
+
 def test_minmax():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90, 91, 93, 95, 99]
-    test_arr = 1
+    input_arr = [3.0, 6.0, 7.0, 8.0, 4.0]
+    test_arr = [3.0, 8.0]
 
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+    result = find_min_max(input_arr)
+
+    assert (result == test_arr)
+
+def test_avg():
+    result = []
+    input_arr = [3.0, 7.0, 8.0, 4.0, 5.0]
+    test_arr = 5.4
+
+    result = calc_average(input_arr)
+
+    assert (result == test_arr)
+
+def test_median():
+    result = []
+    input_arr = [3.0, 4.0, 5.0, 7.0, 8.0]
+    test_arr = 5.0
+
+    result = calc_median_temperature(input_arr)
 
     assert (result == test_arr)
