eb2bd096a1c32ea09786f728b906462ab80fe944
Jovian Heng <jovianheng.24@ichat.sp.edu.sg>
2025-12-03 07:16:42 +0800

    pytest func employee data

--- Test_employee_info.py
+++ Test_employee_info.py
@@ -1,9 +1,19 @@
-from employee_info import get_employees_by_age_range
+import employee_info
 
 
-def test_total_cost_shopping():
+def test_get_employees_by_age_range():
     result = []
     test_arr = [{'name': 'Jane', 'age': 25, 'department': 'Marketing', 'salary': 60000}]
-    result = get_employees_by_age_range(24, 26)
+    result = employee_info.get_employees_by_age_range(24, 26)
     assert (result == test_arr)
 
+def test_calculate_average_salary():
+    result = int(employee_info.calculate_average_salary())
+    assert (result == 60166)
+
+def test_get_employees_by_dept():
+    result = []
+    department = "Marketing"
+    test_arr = [{'name': 'Jane', 'age': 25, 'department': 'Marketing', 'salary': 60000},{"name": "Mary", "age": 23, "department": "Marketing", "salary": 56000}]
+    result = employee_info.get_employees_by_dept(department)
+    assert (result == test_arr)
